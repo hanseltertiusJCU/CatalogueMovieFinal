452489891c1688a06d0a5fc68f881c84daf005dd
Hansel Tertius <hansel.tertius@my.jcu.edu.au>
2019-03-29 15:32:46 +0700

    Final commit

--- app/src/main/java/com/example/cataloguemoviefinal/receiver/UpdateWidgetDataReceiver.java
+++ app/src/main/java/com/example/cataloguemoviefinal/receiver/UpdateWidgetDataReceiver.java
@@ -5,24 +5,24 @@ import android.content.BroadcastReceiver;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
-import android.util.Log;
 
 import com.example.cataloguemoviefinal.R;
 import com.example.cataloguemoviefinal.widget.FavoriteMovieItemWidget;
 
+/**
+ * Kelas ini berguna untuk menerima pesan dari app FavoriteFilmApp dan
+ * berguna untuk update widget dari FavoriteFilmApp bahkan dalam kondisi ketika app ini tidak dibuka
+ * (applied to API lower than 26)
+ */
 public class UpdateWidgetDataReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        // TODO: This method is called when the BroadcastReceiver is receiving
-        // an Intent broadcast.
-        Log.d("Action intent", intent.getAction());
 
-        if(intent.getAction() != null){
-            if(intent.getAction().equals("com.example.cataloguemoviefinal.ACTION_UPDATE_WIDGET_DATA")){
-                // Log message
-                Log.d("Testing receiver", "I got ur updates");
-
+        if (intent.getAction() != null) {
+            // Cek jika actionnya itu sama dengan action yang ada di Intent di app FavoriteFilmApp
+            if (intent.getAction().equals("com.example.cataloguemoviefinal.ACTION_UPDATE_WIDGET_DATA")) {
+                // Update widget content
                 AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(context);
 
                 int[] appWidgetIds = appWidgetManager.getAppWidgetIds(new ComponentName(context, FavoriteMovieItemWidget.class));
